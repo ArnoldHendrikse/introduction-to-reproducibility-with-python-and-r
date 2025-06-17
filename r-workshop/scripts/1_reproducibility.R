b393b91bdbea73de1d604daee3ce8c773fcf4883
pedrohserrano <hdez.serrano@gmail.com>
2025-06-17 11:53:22 +0200

    directory adjusted

--- r-workshop/scripts/1_reproducibility.R
+++ r-workshop/scripts/1_reproducibility.R
@@ -20,5 +20,5 @@ results <- broom::tidy(model)
 
 # SAVE RESULTS ---- 
 timestamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
-write_xlsx(results, paste0("../results/regression_result_", timestamp, ".xlsx"))
+write_xlsx(results, paste0("regression_result_", timestamp, ".xlsx"))
 cat("✔ Done! Regression results saved in results folder.\n")
